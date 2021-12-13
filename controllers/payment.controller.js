7acd08e674eb24abc50a268874bd2e2d2fa702b5
Silvestre Moreno <silmoreno70@gmail.com>
2021-12-12 18:55:40 -0600

    first commit

---
+++ controllers/payment.controller.js
@@ -0,0 +1,28 @@
+const PAYMENT_FILE_PATH = 'payment-generated';
+const faker = require('faker');
+const fs = require('fs');
+const LINE_ENDING = require('os').EOL;
+
+module.exports = {
+    create: function (req, res) {
+        const fd = fs.openSync(PAYMENT_FILE_PATH, 'a');
+        fs.appendFileSync(fd, faker.commerce.price() + LINE_ENDING, 'utf8');
+        res.status(201).send();
+    },
+
+    applyDiscount: function (req, res) {
+        //debera de restar una cantidad a cada precio en payment-generated.txt
+        res.json({ message: ""});
+    },
+
+    getPromos: function (req, res) {
+        // req to res
+        res.json([
+            {name: "BUENFIN"},
+            {name: "HOTSALE"},
+            {name: "CYBERMONDAY"},
+            {name: "BLACKFRIDAY"},
+            {name: "PRIMEDAY"},
+        ]);
+    }
+};
