b5a8bed2e3a3c193ef8989e8c71c93f2b4491575
Silvestre Moreno <silmoreno70@gmail.com>
2021-12-15 03:38:19 -0600

    feat: add hot-shots (for datadog)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /* Agregar counter */
+  dogstatsd.increment('index');
   res.send('<p>HTML Data</p>');
 });
 
